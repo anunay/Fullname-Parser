56ddedef59d0c5189998e36206e8d6ca04fa3124
Anunay Dahal <anunay.dahal@gmail.com>
2013-08-28 09:56:10 +0545

    UnitTest created

---
+++ tests/FullnameParserTest.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'parser.php';
+
+class FullnameParserTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @group Travis
+     */
+    public function testGetFullnameParts()
+    {
+
+        $original_name = "Mr. Anunay K. Dahal II";
+        $parser = new FullnameParser($original_name);
+        $name_parts = $parser->getNamePartials();
+        $fullname = import(" ",$name_parts);
+        $original_name_block_count = count(explode(" ",$original_name));
+        $parsed_name_block_count = count($name_parts);
+
+        $this->assertEquals($original_name, $fullname);
+        $this->assertEquals($original_name_block_count,$parsed_name_block_count);
+    }
+
+}
+?>
